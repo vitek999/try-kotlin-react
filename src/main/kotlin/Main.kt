bf940af1cfe7865dedeeb0ecd33398b2f52a9aed
vitek999 <vitek2012rus@gmail.com>
2020-04-29 00:50:41 +0400

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+import react.dom.*
+import kotlin.browser.document
+
+val unwatchedVideos = listOf(
+    Video(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
+    Video(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
+    Video(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
+)
+
+val watchedVideos = listOf(
+    Video(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
+)
+
+fun main() {
+    render(document.getElementById("root")) {
+        child(App::class) {  }
+    }
+}
