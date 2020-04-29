56464cc0ab37d041fdcdf3ce7a092152710662bf
vitek999 <vitek2012rus@gmail.com>
2020-04-29 09:42:51 +0400

    Added React player

--- src/main/kotlin/VideoPlayer.kt
+++ src/main/kotlin/VideoPlayer.kt
@@ -40,10 +40,8 @@ class VideoPlayer : RComponent<VideoPlayerProps, RState>() {
                     +"Mark as unwatched"
                 }
             }
-            img {
-                attrs {
-                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
-                }
+            ReactPlayer {
+                attrs.url = props.video.videoUrl
             }
         }
     }
